8f4277f102865abfab97c6a81c483e86236b6a46
Brady Wied <brady@bswtechconsulting.com>
2022-03-09 15:21:13 -0700

    Attempt to do this

---
+++ index.ts
@@ -0,0 +1,19 @@
+import { paths } from "./jira";
+import { Fetcher } from "openapi-typescript-fetch";
+import './polyfills'
+
+const fetcher = Fetcher.for<paths>()
+fetcher.configure({
+    baseUrl: "https://bswtechconsulting.atlassian.net",
+    init: {
+        headers: {
+
+        }
+    }
+})
+
+const getIssueById = fetcher.path("/rest/api/3/issue/{issueIdOrKey}").method("get").create()
+
+getIssueById({issueIdOrKey: "CAK-76"}).then(stuff => {
+    console.log(stuff.data.fields["summary"]);
+})
